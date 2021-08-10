5a43f555a86418ca63ff5fce42501fc5cdadb652
Polar Bear <polarbear_avax@protonmail.com>
2021-08-10 18:15:42 -0300

    Create EverestStandardOracle.sol

---
+++ contracts/EverestStandardOracle.sol
@@ -0,0 +1,24 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity 0.6.12;
+
+import "./standards/IERC20.sol";
+import "./standards/Context.sol";
+import "./standards/SafeMath.sol";
+import "./standards/Address.sol";
+import "./standards/ERC20.sol";
+import "./standards/Ownable.sol";
+import "./standards/Address.sol";
+import "./standards/Address.sol";
+
+contract EverestStandardOracle is Ownable {
+    uint256 public _Expired;
+    event ValueChanged(uint256 Expired);
+    function store(uint256 Expired) public onlyOwner {
+        _Expired = Expired;
+        emit ValueChanged(Expired);
+    }
+    function retrieve() public view returns (uint256) {
+        return _Expired;
+    }
+}
